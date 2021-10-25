20bd37475ab38ea4fb63f4947517c604473ff41a
KevinEs14 <escobarkev@gmail.com>
2021-10-25 18:58:35 -0400

    creation of posInicial

--- src/main/java/Ascensor.java
+++ src/main/java/Ascensor.java
@@ -1,3 +1,24 @@
 
 public class Ascensor {
+    int posAscensor=1;
+    public int CrearPersona()throws  Exception{
+        int aleatorio=(int)Math.random()*2+1;
+        int aleatorio2=(int)Math.random()*2+1;
+
+
+        return aleatorio;
+    }
+    public int DestinoPersona(int aleatorio)throws  Exception{
+        int aleatorio2=(int)Math.random()*2+1;
+        if(aleatorio2==aleatorio){aleatorio2=(int)Math.random()*2+1;}
+        return aleatorio2;
+    }
+    public int posInicial(boolean dia) {
+        if(dia){
+            posAscensor=1;
+            return posAscensor;
+        }else{
+            return posAscensor;
+        }
+    }
 }
