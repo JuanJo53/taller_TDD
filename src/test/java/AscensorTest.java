97c148f4fea762537dac6dce57f840e1d8b3cb67
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-10-25 18:58:34 -0400

    test InicioDia

--- src/test/java/AscensorTest.java
+++ src/test/java/AscensorTest.java
@@ -1,5 +1,16 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AscensorTest {
+    @Test
+    public void siInicioDia()throws Exception{
+        //1. Preparacion de la prueba
+        Ascensor asc = new Ascensor();
+        //2. Logica de la prueba
+        int resultado = asc.posIncial(true);
+        //3. Verificacion o assert
+        assertEquals("1",resultado);
+    }
 
 }
