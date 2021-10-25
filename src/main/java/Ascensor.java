4f32b47ef0a80d0046de7cb4dbbcbdb31961d01c
KevinEs14 <escobarkev@gmail.com>
2021-10-25 19:36:51 -0400

    creation of llamarAscensor and verificarDestino

--- src/main/java/Ascensor.java
+++ src/main/java/Ascensor.java
@@ -3,9 +3,6 @@ public class Ascensor {
     int posAscensor=1;
     public int CrearPersona()throws  Exception{
         int aleatorio=(int)Math.random()*2+1;
-        int aleatorio2=(int)Math.random()*2+1;
-
-
         return aleatorio;
     }
     public int DestinoPersona(int aleatorio)throws  Exception{
@@ -21,4 +18,21 @@ public class Ascensor {
             return posAscensor;
         }
     }
+    public String llamarAscensor(int piso){
+        if(piso==posAscensor){
+            return "EL ascensor ya se encuentra en ese piso";
+        }else{
+            posAscensor=piso;
+            return "El ascensor ya va en camino";
+        }
+    }
+    public String verificarDestino(int piso){
+        if(piso==posAscensor || piso<1 || piso>3){
+            return "El piso al que desea ir no es valido";
+        }else{
+            posAscensor=piso;
+            return "El ascensor ya va en camino";
+        }
+    }
+
 }
